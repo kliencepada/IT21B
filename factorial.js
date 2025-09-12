5e45557e57b6afba94562166fdb7a97a9ba8cdfe
kliencepada <jirehjamesc@gmail.com>
2025-09-12 11:22:25 +0800

    added mainMenu function

--- factorial.js
+++ factorial.js
@@ -4,3 +4,17 @@ const rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 })
+
+
+function mainMenu(){
+   console.log("-------------------------");
+   console.log("1. Say Hello ");
+   console.log("2. Factorial");
+   console.log("3. Exit Application");
+   console.log("-------------------------");
+
+rl.question("Enter your Choice (1-3): ", choice =>{
+    console.log(choice);
+
+})
+}
