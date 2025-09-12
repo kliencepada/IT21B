7d3b3888355a020305d7e727fdc3426ffe99b247
kliencepada <jirehjamesc@gmail.com>
2025-09-12 11:24:10 +0800

    added sayHello function

--- factorial.js
+++ factorial.js
@@ -29,3 +29,31 @@ rl.question("Enter your Choice (1-3): ", choice =>{
     }
 })
 }
+
+function sayHello(){
+     console.log("Hello");
+     backToMenu();
+}
+
+function computeFactorial(){
+         console.log("Factorial");
+
+        rl.question("Please enter a number for factorial: ", numStr =>{
+            let num = parseInt(numStr);
+
+            if (isNaN(num) || num < 0){
+                console.log("Please enter a non-negative integer");
+                backToMenu();
+            } else {
+                let fact = 1;
+
+                for (let i=1; i <= num; i++){
+                    fact *=i;
+                }
+
+                console.log("The factorial of" + num + " is " + fact);
+                backToMenu();
+            }
+        })
+}
+
