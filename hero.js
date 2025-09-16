88c92d2535648f68567a348d7c2fa5faa740d0f2
kliencepada <jirehjamesc@gmail.com>
2025-09-16 08:36:36 +0800

    Added Mage class as inherited from Hero class

--- hero.js
+++ hero.js
@@ -29,7 +29,21 @@ class Warrior extends Hero{
     }
 }
 
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+
+    useAbility(){
+        console.log('${this.name} uses Fireball!');
+        }
+    }
+
 const Thorin = new Warrior("Thorin",100,10);
-const hero2 = new Hero("Gandalf",100,8);
+const Gandalf = new Mage("Gandalf",100,8,50);
 
 Thorin.useAbility();
+Thorin.showStats();
+Gandalf.showStats();
+Gandalf.useAbility();
