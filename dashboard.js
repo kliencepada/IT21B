0bcc826a92dbef7eef658ea6c95a2e7bf9825b45
kliencepada <jirehjamesc@gmail.com>
2025-10-28 07:59:39 +0800

    Added the script from linechart.html

--- dashboard.js
+++ dashboard.js
@@ -63,4 +63,64 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 
 
-    
+    class LineChart {
+        constructor (canvasId, dataUrl){
+            this.canvasId = camvasId;
+            this.dataUrl = dataUrl;
+            this.chart = null;
+        }
+
+        renderChart(data){
+            const ctx = document.getElementById(this.canvasId).getContext("2d");
+
+            //
+            this.chart = new Chart(ctx,{
+                type:"line",
+                data:{
+                    labels: data.labels,
+                    datasets:[{
+                        label:"Monthly Data",
+                        data: data.values,
+                        borderwidth:1
+
+                    }]
+                },
+
+                options: {
+                   scales: {
+                     y: {
+                     beginAtZero: true
+                  }
+                }
+              }
+            });
+            //
+
+        }
+
+        async fetchData(){
+            try{
+                const response = await fetch(this.dataUrl);
+
+                if(!response.ok) throw new Error(`Failed to load data:${response.statusText}`);
+                const data = await response.json();
+                return data;
+
+            }catch(error){
+                console.error("Error fetching data:", error);
+                return null;
+
+            }
+        }
+
+        async init(){
+            const data = await this.fetchData();
+            if(data){
+                this.renderChart(data);
+            }
+        }
+    }
+document.addEventListener("DOMContentLoaded", () =>{
+    const chart = new LineChart("linechart","linedata.json");
+    chart.init();
+});
